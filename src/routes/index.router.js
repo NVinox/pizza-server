7c806ecb653fdf9074fb11ed53935378cf4e4b56
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2024-05-18 20:24:02 +0700

    feat(DoughRouter): создал роутер для типа продукта

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -2,10 +2,12 @@ import { Router } from "express"
 
 import categoriesRouter from "./categories.router.js"
 import sortCategoriesRouter from "./sortCategories.router.js"
+import doughRouter from "./dough.router.js"
 
 const router = Router()
 
 router.use("/categories", categoriesRouter)
 router.use("/sort-categories", sortCategoriesRouter)
+router.use("/dough", doughRouter)
 
 export default router
