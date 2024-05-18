cd8cee05e026a9c285858ff22a544d23cdc3cbe2
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2024-05-18 17:22:48 +0700

    feat(Category): метод добавления категории

--- src/controllers/Categories.controller.js
+++ src/controllers/Categories.controller.js
@@ -9,13 +9,42 @@ export class CategoriesController {
     this._prisma = new PrismaClient()
   }
 
-  getCategories = async (req, res, next) => {
+  getAll = async (req, res, next) => {
     try {
       const categories = await this._prisma.category.findMany()
 
       return res
         .status(200)
         .json(new HttpResponse("OK", "OK", "Categories list", categories))
+    } catch (error) {
+      return next(ApiError.internal())
+    }
+  }
+
+  create = async (req, res, next) => {
+    try {
+      const categoryTitle = req.body.text
+
+      if (!categoryTitle) {
+        return next(new ApiError(400, "Filed 'text' is required"))
+      }
+
+      const category = await this._prisma.category.create({
+        data: {
+          text: categoryTitle,
+        },
+      })
+
+      return res
+        .status(201)
+        .json(
+          new HttpResponse(
+            "CREATED",
+            "CREATED",
+            "Category is created",
+            category
+          )
+        )
     } catch (error) {
       return next(ApiError.internal())
     }
--- src/routes/categories.router.js
+++ src/routes/categories.router.js
@@ -5,6 +5,7 @@ import { CategoriesController } from "../controllers/Categories.controller.js"
 const categoriesRouter = Router()
 const categoriesController = new CategoriesController()
 
-categoriesRouter.get("/", categoriesController.getCategories)
+categoriesRouter.post("/", categoriesController.create)
+categoriesRouter.get("/", categoriesController.getAll)
 
 export default categoriesRouter
