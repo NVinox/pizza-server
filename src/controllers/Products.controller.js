0cf38846bda96f6d8ff98a34727babe335b5d510
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2024-05-19 18:23:21 +0700

    feat(ProductsController): добавил эндпоинт создания продукта

--- src/controllers/Products.controller.js
+++ src/controllers/Products.controller.js
@@ -1,20 +1,61 @@
+import { Files } from "../services/Files.service.js"
+
 import { HttpResponse } from "../domain/index.domain.js"
 import { ApiError } from "../error/index.error.js"
-import { getEmptyFields } from "../helpers/index.helper.js"
 
+import { getEmptyFields } from "../helpers/index.helper.js"
 import { PrismaClient, Prisma } from "@prisma/client"
 
 export class ProductsController {
   _prisma
+  _filesService
 
   constructor() {
     this._prisma = new PrismaClient()
+    this._filesService = new Files()
+  }
+
+  create = async (req, res, next) => {
+    try {
+      if (req.files) {
+        const image = this._filesService.saveFile(req.files.image)
+        const raiting = req.body.raiting ? +req.body.raiting : null
+        const body = {
+          title: req.body.title,
+          description: req.body.description,
+          image,
+          raiting,
+          basePrice: +req.body.basePrice,
+        }
+        const emptyFields = getEmptyFields(body)
+
+        if (emptyFields) {
+          return next(
+            ApiError.badRequest(`Fields '${emptyFields}' is required`)
+          )
+        }
+        const product = await this._prisma.product.create({ data: body })
+
+        return res
+          .status(201)
+          .json(new HttpResponse("OK", "OK", "Products list", product))
+      }
+
+      return next(ApiError.badRequest(`Fields 'image' is required`))
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        return next(ApiError.throwKnownError(error.code, error.meta))
+      }
+
+      return next(ApiError.internal())
+    }
   }
 
   getAll = async (req, res, next) => {
     try {
       const products = await this._prisma.product.findMany({
         orderBy: { id: "asc" },
+        include: { categories: true, sizes: true, types: true },
       })
 
       if (!products.length) {
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -4,6 +4,7 @@ import { ProductsController } from "../controllers/Products.controller.js"
 const productsController = new ProductsController()
 const productRouter = Router()
 
+productRouter.post("/", productsController.create)
 productRouter.get("/", productsController.getAll)
 
 export default productRouter
