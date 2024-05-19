f91a0331fb5b359e1f99fa536637344f70349042
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2024-05-19 12:26:59 +0700

    feat(SizeRouter): создал роутер размеров

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -3,11 +3,13 @@ import { Router } from "express"
 import categoriesRouter from "./categories.router.js"
 import sortCategoriesRouter from "./sortCategories.router.js"
 import doughRouter from "./dough.router.js"
+import sizeRouter from "./size.router.js"
 
 const router = Router()
 
 router.use("/categories", categoriesRouter)
 router.use("/sort-categories", sortCategoriesRouter)
 router.use("/dough", doughRouter)
+router.use("/sizes", sizeRouter)
 
 export default router
---
+++ src/routes/size.router.js
@@ -0,0 +1,5 @@
+import { Router } from "express"
+
+const sizeRouter = Router()
+
+export default sizeRouter
